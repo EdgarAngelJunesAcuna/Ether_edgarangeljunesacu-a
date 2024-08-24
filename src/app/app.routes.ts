c5ff325c8a9e043289d424cc49dc9bee0798f561
Edgar Angel Raziel Junes Acuña <111781870+EdgarAngelJunesAcuna@users.noreply.github.com>
2024-08-23 19:50:19 -0500

    tree-commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { Web3JsComponent } from './web3-js/web3-js.component'; // Importa el componente Web3
+import { TruffleComponent } from './truffle/truffle.component';
 
 export const routes: Routes = [
-  { path: 'web3', component: Web3JsComponent }, // Ruta para Web3.js
-  { path: '', redirectTo: '/web3', pathMatch: 'full' }, // Redirección inicial a 'web3'
-  { path: '**', redirectTo: '/web3' } // Redirección para rutas no encontradas
+  { path: 'truffle', component: TruffleComponent },
+  { path: '', redirectTo: '/truffle', pathMatch: 'full' },
+  { path: '**', redirectTo: '/truffle' }
 ];
--- src/app/truffle/truffle.component.spec.ts
+++ src/app/truffle/truffle.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Web3JsComponent } from './web3-js.component';
+import { TruffleComponent } from './truffle.component';
 
-describe('Web3JsComponent', () => {
-  let component: Web3JsComponent;
-  let fixture: ComponentFixture<Web3JsComponent>;
+describe('TruffleComponent', () => {
+  let component: TruffleComponent;
+  let fixture: ComponentFixture<TruffleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Web3JsComponent]
+      imports: [TruffleComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Web3JsComponent);
+    fixture = TestBed.createComponent(TruffleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
